24f2fd55b303d1414c3bfa1fd22e0f695d872f90
pertooo <pertiapertia@gmail.com>
2020-04-04 13:45:32 -0400

    FireBase push notifications

--- app/src/main/java/com/example/navitruck/screens/main/MainActivity.java
+++ app/src/main/java/com/example/navitruck/screens/main/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.navitruck.screens.main;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.navitruck.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,18 +29,63 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
+
     private Spinner spinnerDriverStatus;
     private Spinner spinnerTaskType;
     private TextView textTimer;
+
+
+
+    private Button btn_subscribe;
+    private Button btn_unsubscribe;
+
+    private final String TOPIC = "JavaSampleApproach";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        btn_subscribe = (Button) findViewById(R.id.btn_subscribe);
+        btn_unsubscribe = (Button) findViewById(R.id.btn_unsubscribe);
+
+        btn_subscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseMessaging.getInstance().subscribeToTopic(TOPIC);
+            }
+        });
+
+        btn_unsubscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseMessaging.getInstance().unsubscribeFromTopic(TOPIC);
+            }
+        });
+
         initViews();
         setAdapter();
         setListeners();
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+
+
     }
 
     private void initViews(){
@@ -46,17 +104,35 @@ public class MainActivity extends Activity {
                     case 0:
                         spinnerTaskType.setVisibility(View.GONE);
                         textTimer.setText("გამორთული:2 დღე");
+
                         break;
                     case 1:
                         spinnerTaskType.setVisibility(View.VISIBLE);
                         textTimer.setText("ტვირთის გარეშე:36 საათი");
+
+
+//                        FirebaseInstanceId.getInstance().getInstanceId()
+//                                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+//                                    @Override
+//                                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+//                                        if (!task.isSuccessful()) {
+//                                            Log.w(TAG, "getInstanceId failed", task.getException());
+//                                            return;
+//                                        }
+//
+//                                        // Get new Instance ID token
+//                                        String token = task.getResult().getToken();
+//
+//                                        // Log and toast
+//                                        String msg = "Ola lalalaaa";
+//                                        Log.d(TAG, msg);
+//                                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                                    }
+//                                });
+
                         break;
                 }
 
-                if(position==0){
-                    spinnerTaskType.setVisibility(View.GONE);
-                    textTimer.setText("გამორთული:2 დღე");
-                }
             }
 
             @Override
